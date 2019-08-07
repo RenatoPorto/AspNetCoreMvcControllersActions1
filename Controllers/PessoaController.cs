809b7b7984dbd61f768e0f6d76eb89174bb44b3b
Renato Rodrigues <renato_rodrigues2@yahoo.com.br>
2019-08-06 22:02:25 -0400

    Formulario

---
+++ Controllers/PessoaController.cs
@@ -0,0 +1,36 @@
+﻿using AspNetCoreMvcControllersActions.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreMvcControllersActions.Controllers
+{
+    public class PessoaController : Controller
+    {
+        public IActionResult Index()
+        {
+            Pessoa p = new Pessoa();
+            p.Nome = "Renato";
+
+            p.Telefone = "65-9522222";
+
+
+            return View(p);
+        }
+
+
+        public IActionResult Formulario()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Formulario(Pessoa pessoa)
+        {
+            return View("Pessoa", pessoa);
+        }
+
+    }
+}
+
